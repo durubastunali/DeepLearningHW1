b920c56248b9f9b04ae2c6305486df70870e7e8b
Duru Baştunalı <durubastunali@gmail.com>
2025-03-22 16:45:48 +0300

    problem2 updates2 -> 'Result too large'

--- problem2.py
+++ problem2.py
@@ -10,9 +10,9 @@ def plot_regression_tenth_order(params):
     plt.scatter(x_values, y_values, color='red', label='Data Points')
 
     x_line = np.linspace(min(x_values), max(x_values), 100)
-    y_line = (params[0] * x_line**9 + params[1] * x_line**8 + params[2] * x_line**7 +
-              params[3] * x_line**6 + params[4] * x_line**5 + params[5] * x_line**4 +
-              params[6] * x_line**3 + params[7] * x_line**2 + params[8] * x_line + params[9])
+    y_line = (params[0] * x_line**10 + params[1] * x_line**9 + params[2] * x_line**8 + params[3] * x_line**7 +
+              params[4] * x_line**6 +  params[5] * x_line**5 + params[6] * x_line**4 + params[7] * x_line**3 +
+              params[8] * x_line**2 +  params[9] * x_line + params[10])
 
     plt.plot(x_line, y_line, color='blue', label=f'Regression Line: 10th Order Polynomial')
 
@@ -20,7 +20,6 @@ def plot_regression_tenth_order(params):
     plt.ylabel('Y')
     plt.legend()
     plt.title('Tenth-Order Polynomial Regression')
-    plt.savefig("tenth_order_regression.png")
     plt.savefig("tenth_order.png")
 
 
@@ -55,20 +54,20 @@ def partial_derivative_b_linear(a_n, b_n):
 
 
 def calculate_parameters_tenth_order():
-    a_0 = np.random.uniform(-10, 10)
-    b_0 = np.random.uniform(-10, 10)
-    c_0 = np.random.uniform(-10, 10)
-    d_0 = np.random.uniform(-10, 10)
-    e_0 = np.random.uniform(-10, 10)
-    f_0 = np.random.uniform(-10, 10)
-    g_0 = np.random.uniform(-10, 10)
-    h_0 = np.random.uniform(-10, 10)
-    i_0 = np.random.uniform(-10, 10)
-    j_0 = np.random.uniform(-10, 10)
-    k_0 = np.random.uniform(-10, 10)
-
+    a_0 = np.random.uniform(-5, 5)
+    b_0 = np.random.uniform(-5, 5)
+    c_0 = np.random.uniform(-5, 5)
+    d_0 = np.random.uniform(-5, 5)
+    e_0 = np.random.uniform(-5, 5)
+    f_0 = np.random.uniform(-5, 5)
+    g_0 = np.random.uniform(-5, 5)
+    h_0 = np.random.uniform(-5, 5)
+    i_0 = np.random.uniform(-5, 5)
+    j_0 = np.random.uniform(-5, 5)
+    k_0 = np.random.uniform(-5, 5)
 
     e = 0.0001
+    params = []
 
     while True:
         grads = []
@@ -95,8 +94,9 @@ def calculate_parameters_tenth_order():
         j_n = j_0 - e * grads[9]
         k_n = k_0 - e * grads[10]
 
-        if (abs(a_n - a_0) + abs(b_n - b_0) + abs(c_n - c_0) + abs(d_n - d_0) + abs(e_n - e_0) + abs(f_n - f_0) +
-            abs(g_n - g_0) + abs(h_n - h_0) + abs(i_n - i_0) + abs(j_n - j_0) + abs(k_n - k_0)) < e:
+        if ((a_n - a_0)**2 + (b_n - b_0)**2 + (c_n - c_0)**2 + (d_n - d_0)**2 + (e_n - e_0)**2 + (f_n - f_0)**2 +
+            (g_n - g_0)**2 + (h_n - h_0)**2 + (i_n - i_0)**2 + (j_n - j_0)**2 + (k_n - k_0)**2) < e:
+            params = [a_n, b_n, c_n, d_n, e_n, f_n, g_n, h_n, i_n, j_n, k_n]
             break
         else:
             a_0 = a_n
@@ -124,7 +124,7 @@ def calculate_parameters_linear():
         a_n = a_0 - e * partial_derivative_a_linear(a_0, b_0)
         b_n = b_0 - e * partial_derivative_b_linear(a_0, b_0)
 
-        if abs(a_n - a_0) + abs(b_n - b_0) < e:
+        if (a_n - a_0)**2 + (b_n - b_0)**2 < e:
             print("a =", a_n, "b =", b_n)
             break
         else:
