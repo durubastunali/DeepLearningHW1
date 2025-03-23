c4b10531be40f146de9372d42c13dea7ac02cff0
Duru Baştunalı <durubastunali@gmail.com>
2025-03-23 14:42:05 +0300

    problem3 implemented

---
+++ problem3.py
@@ -0,0 +1,64 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+dataset = [(10, 0), (20, 0), (15, 0), (40,0), (50, 1), (60, 0), (60, 1), (70,1), (80,0), (90, 1), (95, 1), (100,1), (100, 1)]
+
+
+def sigmoid(i, p, q):
+    return 1 / (1 + np.exp(-(p*i + q)))
+
+
+def gradient_p(p, q):
+    sum = 0
+    for instance in dataset:
+        x, y = instance[0], instance[1]
+        sum += (sigmoid(x, p, q) - y) * x
+    return sum
+
+
+def gradient_q(p, q):
+    sum = 0
+    for instance in dataset:
+        x, y = instance[0], instance[1]
+        sum += (sigmoid(x, p, q) - y)
+    return sum
+
+
+def logistic_regression():
+    p = np.random.uniform(0, 1)
+    q = np.random.uniform(0, 1)
+
+    e = 0.01
+
+    while True:
+        p_new = p - e * gradient_p(p, q)
+        q_new = q - e * gradient_q(p, q)
+
+        if (p - p_new)**2 + (q - q_new)**2 < e:
+            print("p:", p_new, "q:", q_new)
+            break
+        else:
+            p = p_new
+            q = q_new
+    return p, q
+
+
+def plot_sigmoid(p, q):
+    x_values = [point[0] for point in dataset]
+    y_values = [point[1] for point in dataset]
+    plt.scatter(x_values, y_values, color='red', label='Data Points')
+
+    x_line = np.linspace(min(x_values), max(x_values), 100)
+    y_line = 1 / (1 + np.exp(-(p*x_line + q)))
+    plt.plot(x_line, y_line, color='blue', label=f'Regression Line: y={p:.2f}x+{q:.2f}')
+
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.title('Logistic Regression - Sigmoid')
+    plt.savefig("sigmoid.png")
+
+
+if __name__ == '__main__':
+    p, q = logistic_regression()
+    plot_sigmoid(p, q)
