problem2 gradient fixed

problem2.py
@@ -4,15 +4,14 @@ from matplotlib import pyplot as plt
 dataset = [(1, 3), (2, 7), (3, 7), (4, 11), (5, 14), (6, 21), (7, 18), (8, 18), (9, 19), (10, 23)]
 
 
-def plot_regression_tenth_order(params):
+def plot_regression_tenth_order(a, b, c, d, e, f, g, h, i, j, k):
     x_values = [point[0] for point in dataset]
     y_values = [point[1] for point in dataset]
     plt.scatter(x_values, y_values, color='red', label='Data Points')
 
     x_line = np.linspace(min(x_values), max(x_values), 100)
-    y_line = (params[0] * x_line**10 + params[1] * x_line**9 + params[2] * x_line**8 + params[3] * x_line**7 +
-              params[4] * x_line**6 +  params[5] * x_line**5 + params[6] * x_line**4 + params[7] * x_line**3 +
-              params[8] * x_line**2 +  params[9] * x_line + params[10])
+    y_line = (a * x_line**10 + b * x_line**9 + c * x_line**8 + d * x_line**7 + e * x_line**6 + f * x_line**5 +
+              g * x_line**4 + h * x_line**3 + i * x_line**2 + j * x_line + k )
 
     plt.plot(x_line, y_line, color='blue', label=f'Regression Line: 10th Order Polynomial')
 
@@ -39,64 +38,57 @@ def plot_regression_linear(a, b):
     plt.savefig("ax_b.png")
 
 
-def partial_derivative_a_linear(a_n, b_n):
+def tenth_order(a, b, c, d, e, f, g, h, i, j, k, x, y):
+    val = a*(x**10) + b*(x**9) + c*(x**8) + d*(x**7) + e*(x**6) + f*(x**5) + g*(x**4) + h*(x**3) + i*(x**2) + j*x + k - y
+    return val
+
+
+#a10 b9 c8 d7 e6 f5 g4 h3 i2 j1 k0
+def partial_derivative_tenth(a, b, c, d, e, f, g, h, i, j, k, power):
     grad_a_sum = 0
     for instance in dataset:
-        grad_a_sum += 2 * a_n * (a_n * instance[0] + b_n - instance[1])
+        grad_a_sum += 2 * (instance[0] ** power) ** tenth_order(a, b, c, d, e, f, g, h, i, j, k, instance[0], instance[1])
     return grad_a_sum
 
 
-def partial_derivative_b_linear(a_n, b_n):
-    grad_b_sum = 0
+def partial_derivative_linear(a, b, power):
+    grad_a_sum = 0
     for instance in dataset:
-        grad_b_sum += 2 * (a_n * instance[0] + b_n - instance[1])
-    return grad_b_sum
+        grad_a_sum += 2 * (instance[0] ** power) * (a * instance[0] + b - instance[1])
+    return grad_a_sum
 
 
 def calculate_parameters_tenth_order():
-    a_0 = np.random.uniform(-5, 5)
-    b_0 = np.random.uniform(-5, 5)
-    c_0 = np.random.uniform(-5, 5)
-    d_0 = np.random.uniform(-5, 5)
-    e_0 = np.random.uniform(-5, 5)
-    f_0 = np.random.uniform(-5, 5)
-    g_0 = np.random.uniform(-5, 5)
-    h_0 = np.random.uniform(-5, 5)
-    i_0 = np.random.uniform(-5, 5)
-    j_0 = np.random.uniform(-5, 5)
-    k_0 = np.random.uniform(-5, 5)
+    a_0 = round(np.random.uniform(-1, 1), 2)
+    b_0 = round(np.random.uniform(-1, 1), 2)
+    c_0 = round(np.random.uniform(-1, 1), 2)
+    d_0 = round(np.random.uniform(-1, 1), 2)
+    e_0 = round(np.random.uniform(-1, 1), 2)
+    f_0 = round(np.random.uniform(-1, 1), 2)
+    g_0 = round(np.random.uniform(-1, 1), 2)
+    h_0 = round(np.random.uniform(-1, 1), 2)
+    i_0 = round(np.random.uniform(-1, 1), 2)
+    j_0 = round(np.random.uniform(-1, 1), 2)
+    k_0 = round(np.random.uniform(-1, 1), 2)
 
-    e = 0.0001
-    params = []
+    e = 0.001
 
     while True:
-        grads = []
-        power = 10
+        a_n = a_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 10)
+        b_n = b_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 9)
+        c_n = c_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 8)
+        d_n = d_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 7)
+        e_n = e_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 6)
+        f_n = f_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 5)
+        g_n = g_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 4)
+        h_n = h_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 3)
+        i_n = i_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 2)
+        j_n = j_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 1)
+        k_n = k_0 - e * partial_derivative_tenth(a_0, b_0, c_0, d_0, e_0, f_0, g_0, h_0, i_0, j_0, k_0, 0)
 
-        while power >= 0:
-            grad_sum = 0
-            for instance in dataset:
-                grad_sum += (a_0*(instance[0]**10) + b_0*(instance[0]**9) + c_0*(instance[0]**8) + d_0*(instance[0]**7) +
-                             e_0*(instance[0]**6) + f_0*(instance[0]**5) + g_0*(instance[0]**4) + h_0*(instance[0]**3) +
-                             i_0*(instance[0])**2 + j_0*instance[0] + k_0 - instance[1]) * 2 * (instance[0] ** power)
-            grads.append(grad_sum)
-            power -= 1
 
-        a_n = a_0 - e * grads[0]
-        b_n = b_0 - e * grads[1]
-        c_n = c_0 - e * grads[2]
-        d_n = d_0 - e * grads[3]
-        e_n = e_0 - e * grads[4]
-        f_n = f_0 - e * grads[5]
-        g_n = g_0 - e * grads[6]
-        h_n = h_0 - e * grads[7]
-        i_n = i_0 - e * grads[8]
-        j_n = j_0 - e * grads[9]
-        k_n = k_0 - e * grads[10]
-
-        if ((a_n - a_0)**2 + (b_n - b_0)**2 + (c_n - c_0)**2 + (d_n - d_0)**2 + (e_n - e_0)**2 + (f_n - f_0)**2 +
-            (g_n - g_0)**2 + (h_n - h_0)**2 + (i_n - i_0)**2 + (j_n - j_0)**2 + (k_n - k_0)**2) < e:
-            params = [a_n, b_n, c_n, d_n, e_n, f_n, g_n, h_n, i_n, j_n, k_n]
+        if ((a_n - a_0) ** 2 + (b_n - b_0) ** 2 + (c_n - c_0) ** 2 + (d_n - d_0) ** 2 + (e_n - e_0) ** 2 +
+            (f_n - f_0) ** 2 + (g_n - g_0) ** 2 + (i_n - i_0) ** 2 + (j_n - j_0) ** 2) + (k_n - k_0) ** 2 < e:
             break
         else:
             a_0 = a_n
@@ -111,18 +103,19 @@ def calculate_parameters_tenth_order():
             j_0 = j_n
             k_0 = k_n
 
-    return params
-
+    return a_n, b_n, c_n, d_n, e_n, f_n, g_n, h_n, i_n, j_n, k_n
 
 def calculate_parameters_linear():
-    a_0 = np.random.uniform(-1, 1)
-    b_0 = np.random.uniform(-1, 1)
+    a_0 = round(np.random.uniform(-1, 1), 2)
+    b_0 = round(np.random.uniform(-1, 1), 2)
 
-    e = 0.0001
+    print("Initial a =", a_0, "and b =", b_0)
+
+    e = 0.001
 
     while True:
-        a_n = a_0 - e * partial_derivative_a_linear(a_0, b_0)
-        b_n = b_0 - e * partial_derivative_b_linear(a_0, b_0)
+        a_n = round(a_0 - e * partial_derivative_linear(a_0, b_0, 1), 2)
+        b_n = round(b_0 - e * partial_derivative_linear(a_0, b_0, 0), 2)
 
         if (a_n - a_0)**2 + (b_n - b_0)**2 < e:
             print("a =", a_n, "b =", b_n)
@@ -136,7 +129,7 @@ def calculate_parameters_linear():
 
 if __name__ == '__main__':
     a, b = calculate_parameters_linear()
-    plot_regression_linear(a, b)
+    #plot_regression_linear(a, b)
 
-    params = calculate_parameters_tenth_order()
-    plot_regression_tenth_order(params)
+    a, b, c, d, e, f, g, h, i, j, k = calculate_parameters_tenth_order()
+    plot_regression_tenth_order(a, b, c, d, e, f, g, h, i, j, k)